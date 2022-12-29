53015bfc4ed5f29635c83313f25dff3a558fda4d
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-12-29 13:55:08 -0700

    added images, created portfolio page

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,9 +1,33 @@
 import React from "react";
+import projects from "../../utils/projects";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 
-export default function About() {
+import "./style.css";
+
+function Portfolio() {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <Container>
+      <div className="d-flex justify-content-center m-3">
+        <h2>My Portfolio</h2>
+      </div>
+      <div className="port d-flex flex-wrap justify-content-around align-items-center mt-5">
+        {projects.map((project) => {
+          return (
+            <Card style={{ width: "18rem" }} className="mb-4">
+              <Card.Img variant="top" src={project.image} />
+              <Card.Body className="border-top">
+                <Card.Title>{project.title}</Card.Title>
+                <Card.Text>{project.desc}</Card.Text>
+                <Card.Link href={project.site}>{project.title}</Card.Link>
+                <Card.Link href={project.github}>GitHub</Card.Link>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
+    </Container>
   );
 }
+
+export default Portfolio;
