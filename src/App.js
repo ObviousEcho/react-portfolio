b29bbc4e2f930c664e282a0b1343b74ed64500ca
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-12-29 13:01:39 -0700

    set up routing and basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About/index";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Test</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<About />} />
+        {/* <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Cotact />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
