17fe29722bb5ef2156cbc7baa4add853e4b12a3f
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-12-29 13:43:34 -0700

    added me image, created about page + styling, added helpers/projects to utils

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,9 +1,43 @@
 import React from "react";
+import Image from "react-bootstrap/Image";
+import "./style.css";
 
 export default function About() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <section className="about m-1 m-md-2 m-lg-5 d-lg-flex align-items-xl-center">
+      <div className="profile-pic text-center">
+        <Image src="../images/me-400.jpg" alt="author" />
+      </div>
+      <div>
+        <h2>A success story in the making...</h2>
+        <p>
+          My name is <strong>Andrew Stehno</strong>, I'm a full-stack web
+          developer, I build applications front and back. I currently reside in
+          South Jordan, UT on the outskirts of the tech center that has become
+          Silicone Slopes. That's me casting the firm shadow in the pic.
+        </p>
+        <p>
+          My background is in engineering. I set out to enter the tech industry
+          by earning a certificate in full-stack web-development. The universe
+          felt I had a few lessons to learn through 2020 and my switching
+          careers was put on hold (pandemic/divorce). I spent the next year
+          focusing on my well-being. A lot of yoga, a lot of deep breathing,
+          exercise, and re-connecting with old friends helped me cope through a
+          changing landscape. Unfortunately I wasn't dong much coding during
+          this time.
+        </p>
+        <p>
+          Fast forward a bit. Having reassessed my goals, I knew that I wanted
+          to complete that career change. The structure of a boot-camp served me
+          well once, so I put myself through the program a second time. I've now
+          earned two certificates in full-stack web-development (if 1 is good, 2
+          is better!) and I have a clear outlook on the future. When I'm not
+          building things with code, I enjoy being outdoors with friends. I have
+          a passion for hiking in the mountains and deserts of Utah, as well as
+          sailing in the ocean. I especially enjoy planning extended off shore
+          trips with those closest to me.
+        </p>
+      </div>
+    </section>
   );
 }
---
+++ src/utils/projects.js
@@ -0,0 +1,53 @@
+const projects = [
+    {
+        image: "../images/srvr.png",
+        title: "Srvr",
+        desc: "A fullstack, punny point-of-sale applicaiton for restrauant workers, utilizing the MVC paradigm.",
+        site: "https://rocky-thicket-19978.herokuapp.com/",
+        github: "https://github.com/ObviousEcho/Srvr"
+    },
+    {
+        image: "../images/tech.png",
+        title: "Tech-Blog",
+        desc: "A CMS-style blog allowing developers to write about tech, post thoughts, and comment.",
+        site: "https://nameless-bayou-61480.herokuapp.com/",
+        github: "https://github.com/ObviousEcho/Tech-Blog"
+    },
+    {
+        image: "../images/weather.png",
+        title: "Weather Dashboard",
+        desc: "Search weather forcasts and save favorites for any city using the Open Weather API.",
+        site: "https://obviousecho.github.io/weather-dashboard/",
+        github: "https://github.com/ObviousEcho/weather-dashboard"
+    },
+    {
+        image: "../images/scheduler.png",
+        title: "Work-Day-Scheduler",
+        desc: "Store todo events for specific hours of the day.  Each hour is color coded indicationg past, current, or future.",
+        site: "shttps://obviousecho.github.io/work-day-scheduler/",
+        github: "https://github.com/ObviousEcho/work-day-scheduler"
+    },
+    {
+        image: "../images/note.png",
+        title: "Note taker",
+        desc: "Create, store, and delete notes!  Built with Node.js using an Exrpess server.",
+        site: "https://blooming-ridge-50621.herokuapp.com/notes",
+        github: "https://github.com/ObviousEcho/note-taker"
+    },
+    {
+        image: "../images/movie.png",
+        title: "Movie Buff",
+        desc: "Search for movies and review using third party Api's.  Save movies to your watch list for later.",
+        site: "https://obviousecho.github.io/movie-buff/",
+        github: "https://github.com/ObviousEcho/movie-buff"
+    },
+    {
+        image: "../images/password.png",
+        title: "Password Generator",
+        desc: "Generate random and secure passwords by answering a series of user prompts.",
+        site: "https://obviousecho.github.io/password-generator/",
+        github: "https://github.com/ObviousEcho/password-generator"
+    }
+];
+
+export default projects;
